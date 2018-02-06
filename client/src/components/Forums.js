e9bdfd6f93e0439acb3ba96529b8b99213c21a68
Peter Choi <phc284@gmail.com>
2018-02-05 21:13:41 -0600

    finish header section

---
+++ client/src/components/Forums.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Forums extends Component {
+  render() {
+    return (
+      <div className="Forums">
+
+      </div>
+    );
+  }
+}
+
+export default Forums;
---
+++ client/src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <div className="headerText">
+          <h1>Dallas Cowboys Fan Resource Page</h1>
+          <h4>Find all of your online Dallas Cowboys needs</h4>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
