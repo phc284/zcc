05f868dbefdaa3e687d74010ac035e4aeb6a144d
Peter Choi <phc284@gmail.com>
2018-02-09 11:34:41 -0600

    add static path in server

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
-
 const app = express();
-const port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//On the server this will serve up our build folder as static files
+app.use(express.static(path.join(__dirname, './client/build')))
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello' });
 });
 
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
