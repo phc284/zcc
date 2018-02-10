cc8524cefb38b6f99f60732bb7949c6472c51182
Peter Choi <phc284@gmail.com>
2018-02-10 16:01:05 -0600

    change header button text, add Info component

---
+++ client/src/components/Info.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Info extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <span>The Eagles still suck</span>
+      </div>
+    );
+  }
+}
+
+export default Info;
