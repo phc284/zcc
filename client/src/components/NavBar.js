dacbacc27f69dc574c34c346067736ae0e806d32
Peter Choi <phc284@gmail.com>
2018-02-10 18:47:58 -0600

    reorder css

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -15,21 +15,3 @@ export default class NavBar extends Component {
     );
   }
 }
-
-              //  <UncontrolledDropdown nav inNavbar>
-              //   <DropdownToggle nav caret>
-              //     Options
-              //   </DropdownToggle>
-              //   <DropdownMenu >
-              //     <DropdownItem>
-              //       Option 1
-              //     </DropdownItem>
-              //     <DropdownItem>
-              //       Option 2
-              //     </DropdownItem>
-              //     <DropdownItem divider />
-              //     <DropdownItem>
-              //       Reset
-              //     </DropdownItem>
-              //   </DropdownMenu>
-              // </UncontrolledDropdown>
