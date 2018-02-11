28a359e881f00140e5bad47c9e88521be7d67318
Peter Choi <phc284@gmail.com>
2018-02-10 18:31:40 -0600

    remove bootstrap and reactstrap. add nav and fake buttons

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,66 +1,35 @@
-import React from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
+import React, { Component } from 'react';
 
-export default class Example extends React.Component {
+export default class NavBar extends Component {
   constructor(props) {
     super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
   }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
+
   render() {
     return (
       <div className="navbar">
-        <Navbar color="faded" light expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu >
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
-        </Navbar>
+        <button>Home</button>
+        <button>Scores/Stats</button>
       </div>
     );
   }
 }
+
+              //  <UncontrolledDropdown nav inNavbar>
+              //   <DropdownToggle nav caret>
+              //     Options
+              //   </DropdownToggle>
+              //   <DropdownMenu >
+              //     <DropdownItem>
+              //       Option 1
+              //     </DropdownItem>
+              //     <DropdownItem>
+              //       Option 2
+              //     </DropdownItem>
+              //     <DropdownItem divider />
+              //     <DropdownItem>
+              //       Reset
+              //     </DropdownItem>
+              //   </DropdownMenu>
+              // </UncontrolledDropdown>
