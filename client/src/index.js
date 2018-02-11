c649e9d919a6479afec498ff41ea5da424faa58f
Peter Choi <phc284@gmail.com>
2018-02-10 20:31:52 -0600

    move css to components folder

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-// import './style.css';
+import './components/style.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
